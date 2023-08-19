65dc78381b41e34ef29fbcf31f946089bfcf09d6
Joseph Madsen <josephmads@protonmail.com>
2023-08-19 09:07:43 -0500

    fixes React frontend so that it consumes data from FastAPI backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
 function App() {
+  const [basics, setBasic] = useState([])
+
+  useEffect(() => {
+    const fetchAllBasic = async () => {
+        const response = await fetch("/basic/all/")
+        const fetchedBasic = await response.json()
+        setBasic(fetchedBasic)
+    }
+
+    const interval = setInterval(fetchAllBasic, 1000)
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      {basics.map(basic => (
+          <li>
+            <div>Name: {basic.name}</div>
+            <div>Description: {basic.description}</div>
+            <div>Backstory: {basic.backstory}</div>
+            <div>tags: {basic.tags}</div>
+            <br></br>
+          </li>
+      
+      ))}
     </div>
+    </>
   );
 }
 
